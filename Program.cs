4b95e55637cc125a7f63ebd16de0a3ba731395c5
Mohanee <mohanee99@gmail.com>
2020-10-25 21:57:42 +0530

    [mohanee]Refactor .gitignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BSTOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
